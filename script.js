000fd5a3abc9218b5a60a88ce1e78c4cab68bc34
Ashimka <ashimka2010@yandex.ru>
2022-01-23 12:58:51 +0500

    input validation

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const userProfile = {};
 //получение значений из input в обьект
   function addUserProfile() {
 
+ 
     userProfile.name = document.querySelector('#name').value;
     userProfile.lastName = document.querySelector('#lastName').value;
     userProfile.sity = document.querySelector('#sity').value;
@@ -17,8 +18,61 @@ const userProfile = {};
     addUserProfile();
     const out = document.querySelector('.out');
 
+    // проверка !пустой input
+    //name
+    if (document.querySelector('#name').value !== '') {
+      userProfile.name = document.querySelector('#name').value;
+    }  
+    if (document.querySelector('#name').value === '') {
+      document.querySelector('#name').style.border ='2px solid red';
+      
+      return document.querySelector('.out').textContent = `Все поля должны быть заполнены`;
+    } 
+
+    // lastName
+    if (document.querySelector('#lastName').value !== '') {
+      userProfile.name = document.querySelector('#lastName').value;
+    }  
+    if (document.querySelector('#lastName').value === '') {
+      document.querySelector('#lastName').style.border ='2px solid red';
+      
+      return document.querySelector('.out').textContent = `Все поля должны быть заполнены`;
+    }
+
+    //sity
+    if (document.querySelector('#sity').value !== '') {
+      userProfile.name = document.querySelector('#sity').value;
+    }  
+    if (document.querySelector('#sity').value === '') {
+      document.querySelector('#sity').style.border ='2px solid red';
+      
+      return document.querySelector('.out').textContent = `Все поля должны быть заполнены`;
+    }
+
+    //DateBirth
+    if (document.querySelector('#DateBirth').value !== '') {
+      userProfile.name = document.querySelector('#DateBirth').value;
+    }  
+    if (document.querySelector('#DateBirth').value === '') {
+      document.querySelector('#DateBirth').style.border ='2px solid red';
+      
+      return document.querySelector('.out').textContent = `Все поля должны быть заполнены`;
+    }
+
+    //email
+    if (document.querySelector('#email').value !== '') {
+      userProfile.name = document.querySelector('#email').value;
+    }  
+    if (document.querySelector('#email').value === '') {
+      document.querySelector('#email').style.border ='2px solid red';
+      
+      return document.querySelector('.out').textContent = `Все поля должны быть заполнены`;
+    }
+
+    // скрывает анкету
     document.querySelector('.wrap__out').classList.add('reove-block');
 
+    // Выводит данные пользователя на страницу
     out.innerHTML = `
             <h3>Анкета пользователя</h3>
             <p>Имя: ${userProfile.name}</p>
